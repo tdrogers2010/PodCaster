875ae050e20db6b284c2bd78c9f06945f0ad8a55
tdrogers2010 <145821658+tdrogers2010@users.noreply.github.com>
2023-11-05 16:28:10 -0800

    Voice Model Added
    Added my own voice model to the model then connected our code to this model.
    
    Also setup basic gitfunctions to protect vars

--- pylib/ask_11labs.py
+++ pylib/ask_11labs.py
@@ -1,30 +1,52 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-voiceid = "21m00Tcm4TlvDq8ikWAM"
-url = "https://api.elevenlabs.io/v1/text-to-speech/" & voiceid
+# Load environment variables from .env file
+load_dotenv()
+
+# Define the ElevenLabs API endpoint for text-to-speech
+api_version = "v1"
+voiceid = "wTV3G9KHdaVU6P2ZidG7" #Taylor Hoping to Be
+url = f"https://api.elevenlabs.io/{api_version}/text-to-speech/" + voiceid
+
+# Get the ElevenLabs API Key from environment variables
+api_key = os.getenv("ELEVENLABS_API_KEY")
+
+if not api_key:
+    raise ValueError("Error: ELEVENLABS_API_KEY not found in environment variables.")
 
 headers = {
     "Accept": "audio/mpeg",
     "Content-Type": "application/json",
-    "xi-api-key": "51577e00ba74e6b20632c70b407d83f8"
+    "xi-api-key": api_key
 }
 
-data = {
-    "text": "Whhhatttsssuppp!",
-    "model_id": "eleven_monolingual_v1",
-    "voice_settings": {
-        "stability": 0.5,
-        "similarity_boost": 0.5
+def generate_voiceover(text):
+    data = {
+        "text": text,
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {
+            "stability": 0.5,
+            "similarity_boost": 0.5
+        }
     }
-}
 
-response = requests.post(url, json=data, headers=headers)
+    try:
+        # Make the API call
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
 
-# Check if the response status code is 200 (OK)
-if response.status_code == 200:
-    # Write the content in binary mode to a file
-    with open('output.mp3', 'wb') as f:
-        f.write(response.content)
-else:
-    print(f"Failed to generate speech. Status code: {response.status_code}")
-    print(f"Response: {response.text}")
+        # Write the audio content to a file if response is successful
+        with open('output.mp3', 'wb') as f:
+            f.write(response.content)
+        return "Voiceover generated successfully."
+    except requests.RequestException as e:
+        # Handle any errors that occur during the API call
+        raise ConnectionError(f"API request failed: {e}")
+
+# Test the function
+if __name__ == "__main__":
+    input_text = "I'mm the best"
+    result = generate_voiceover(input_text)
+    print(result)
