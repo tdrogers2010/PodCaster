2fc3fc669015f0bec031873ecedca3f8f404b2b0
tdrogers2010 <145821658+tdrogers2010@users.noreply.github.com>
2023-11-05 17:14:35 -0800

    More generative AI

--- pylib/ask_11labs.py
+++ pylib/ask_11labs.py
@@ -5,31 +5,36 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Define the ElevenLabs API endpoint for text-to-speech
-api_version = "v1"
-voiceid = "wTV3G9KHdaVU6P2ZidG7" #Taylor Hoping to Be
-url = f"https://api.elevenlabs.io/{api_version}/text-to-speech/" + voiceid
-
 # Get the ElevenLabs API Key from environment variables
 api_key = os.getenv("ELEVENLABS_API_KEY")
 
 if not api_key:
     raise ValueError("Error: ELEVENLABS_API_KEY not found in environment variables.")
 
-headers = {
-    "Accept": "audio/mpeg",
-    "Content-Type": "application/json",
-    "xi-api-key": api_key
-}
+def generate_voiceover(text, voice_id, similarity_boost=0.75, stability=0.5, style=0.92, use_speaker_boost=True):
+    # Define the ElevenLabs API endpoint for text-to-speech with the voice ID
+    api_version = "v1"
+    url = f"https://api.elevenlabs.io/{api_version}/text-to-speech/{voice_id}"
 
-def generate_voiceover(text):
+    headers = {
+        "Accept": "audio/mpeg",
+        "Content-Type": "application/json",
+        "xi-api-key": api_key
+    }
+
+    # Initialize voice settings with default or provided values
+    voice_settings = {
+        "similarity_boost": similarity_boost,
+        "stability": stability,
+        "style": style,
+        "use_speaker_boost": use_speaker_boost
+    }
+
+    # Data payload for post request
     data = {
         "text": text,
         "model_id": "eleven_monolingual_v1",
-        "voice_settings": {
-            "stability": 0.5,
-            "similarity_boost": 0.5
-        }
+        "voice_settings": voice_settings
     }
 
     try:
@@ -47,6 +52,9 @@ def generate_voiceover(text):
 
 # Test the function
 if __name__ == "__main__":
-    input_text = "I'mm the best"
-    result = generate_voiceover(input_text)
+    input_text = "Hi! My name is Bella, nice to meet you!"
+    voice_id = "21m00Tcm4TlvDq8ikWAM"  # Replace with your voice ID
+
+    # Call the function with default settings
+    result = generate_voiceover(input_text, voice_id)
     print(result)
